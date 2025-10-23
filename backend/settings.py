5683dd2917bf6dc8941d98b43f4087f0eb25a2c7
TekYak <jakov.yoda@gmail.com>
2025-10-23 13:56:10 +0200

    Backend|security: move previously exposed SECRET_KEY to .env, new key generated

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$kp*_af3q%a9cf005y9shmp6%7yj(-wi)^tq=oj3)8@z@(c^#3"
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
