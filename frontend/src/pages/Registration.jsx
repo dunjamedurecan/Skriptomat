edf72cdcac5276d94e217cf0e4ebffd6e1edd2cf
TekYak <jakov.yoda@gmail.com>
2025-11-03 10:18:04 +0100

    Frontend|feat: visual difference for registration from login

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
+import styles from '../styles/Login.module.css'
+import regStyles from '../styles/Registration.module.css'
 
 export default function Registration(){
     const [email, setEmail]=useState('');
@@ -23,18 +25,39 @@ export default function Registration(){
     }
 
     return(
-        <div className="login-container">
-            <div className='login-card'>
-            <h2>Registracija</h2>
-            <form onSubmit={handleSubmit} className="login-form">
-            <label>
-                Email
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="ime@primjer.com" required/>
-            </label>
-            {error && <div className="error">{error}</div>}
-            <button type="submit">Prijavi se</button>
-            <p>Imaš račun,<Link to="/login">prijavi se</Link> </p>
-            </form>
+
+        <div className={styles.loginContainer}>
+            <h1>Registracija</h1>
+            <div className={styles.loginBox}>
+                <form onSubmit={handleSubmit} className={styles.loginForm}>
+                    <div className={styles.formGroup}>
+                        <label>Email or Username</label>
+                        <input 
+                            type='email' 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder='example@fer.hr'
+                        />
+                    </div>
+
+                    <div className={regStyles.passwordInput}>
+                        <div className={styles.formGroup}>
+                            <label>Lozinka</label>
+                            <input type='password' placeholder='••••••••' />
+                        </div>
+
+                        <div className={styles.formGroup}>
+                            <label>Ponovi lozinku</label>
+                            <input type='password' placeholder='••••••••' />
+                        </div>
+                    </div>
+
+                    {error && <p className={styles.error}>{error}</p>}
+
+                    <button type='submit' className={styles.loginButton}>
+                        Registriraj se
+                    </button>
+                </form>
             </div>
         </div>
     );
