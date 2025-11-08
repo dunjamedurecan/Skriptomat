3d59f87fa36e241884d5d63c4f3b89abacf5d302
Dunja Međurečan <dunja.medurecan@unizg.fer.hr>
2025-11-08 14:09:08 +0100

    Backend|remove: secret_key

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','n-lh%^u2k4jp$w!+$v&5czd_*-fq+2ot@xv&&mjlp1nh5=7^ox')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
