84de93a41be3ec057f568d7c506ee0385d6da6ba
DanielaJovanovic <daniela.jovanovic@fer.unizg.hr>
2025-11-13 22:56:52 +0100

    Frontend|add: AuthProvider to main

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './App.css'
 import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
