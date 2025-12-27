782d66b161465fba77599e0b3fceb61833adcd59
Dunja Međurečan <dunja.medurecan@unizg.fer.hr>
2025-12-27 11:46:05 +0100

    Backend|add: diagrams

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "oauth2_provider",
     "corsheaders",
+    "django_extensions",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
